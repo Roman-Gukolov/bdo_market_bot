5b87bd269b53dd7bd658bff9031c7c75956d3afe
Roman Gukolov <roma.gykolov@yandex.com>
2022-03-08 17:24:32 +0300

    add ping task for Heroku deploy

---
+++ src/main/java/com/bdo/telegram/service/PingTask.java
@@ -0,0 +1,30 @@
+package com.bdo.telegram.service;
+
+import lombok.NoArgsConstructor;
+import lombok.SneakyThrows;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Component;
+
+import java.net.HttpURLConnection;
+import java.net.URL;
+
+/**
+ * Task for Heroku. Should provide app not too sleep after 30 min.
+ */
+@Component
+@NoArgsConstructor
+@Slf4j
+public class PingTask {
+    private final static String PING_URL = "https://www.google.com";
+
+    @Scheduled(fixedRate = 1200000)
+    @SneakyThrows
+    public void ping() {
+        URL url = new URL(PING_URL);
+        HttpURLConnection con = (HttpURLConnection) url.openConnection();
+        con.connect();
+        log.info("Ping {}, OK: response code {}", url.getHost(), con.getResponseCode());
+        con.disconnect();
+    }
+}
